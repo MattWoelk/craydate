772735a74a12d8af4626560e04255e227694edf1
MattWoelk <matt.woelk@gmail.com>
2022-06-11 21:38:18 -0500

    Generating a noisy parttern, and removing a lot of unused code.

--- craydate/examples/playground-project/playground/src/lib.rs
+++ craydate/examples/playground-project/playground/src/lib.rs
@@ -3,6 +3,7 @@
 #![feature(never_type)]
 
 use craydate::*;
+use euclid::Trig;
 
 extern crate alloc;
 
@@ -32,19 +33,19 @@ async fn main(mut api: craydate::Api) -> ! {
   let grey50 = Pattern::new_unmasked(grey50_colors);
   graphics.clear(&grey50);
 
-  let mut bmp = Bitmap::new(100, 40, SolidColor::kColorWhite);
-  let mask = Bitmap::new(100, 40, SolidColor::kColorWhite);
-  bmp.set_mask_bitmap(&mask).expect("mask problems");
+  //let mut bmp = Bitmap::new(100, 40, SolidColor::kColorWhite);
+  //let mask = Bitmap::new(100, 40, SolidColor::kColorWhite);
+  //bmp.set_mask_bitmap(&mask).expect("mask problems");
 
-  graphics.draw_bitmap(&bmp, 5, 9, BitmapFlip::kBitmapUnflipped);
+  //graphics.draw_bitmap(&bmp, 5, 9, BitmapFlip::kBitmapUnflipped);
 
-  let mut stencil = Bitmap::new(64, 64, SolidColor::kColorWhite);
-  for y in 0..64 as usize {
-    let c = y % 4 != 0;
-    for x in 0..64 as usize {
-      stencil.as_pixels_mut().set(x, y, c.into());
-    }
-  }
+  //let mut stencil = Bitmap::new(64, 64, SolidColor::kColorWhite);
+  //for y in 0..64 as usize {
+  //  let c = y % 4 != 0;
+  //  for x in 0..64 as usize {
+  //    stencil.as_pixels_mut().set(x, y, c.into());
+  //  }
+  //}
 
   let font = Font::from_file("Mini Sans 2X.pft");
   let _active = match &font {
@@ -64,249 +65,44 @@ async fn main(mut api: craydate::Api) -> ! {
   };
 
   {
-    let _stencil_holder = graphics.set_stencil(&stencil);
+    //let _stencil_holder = graphics.set_stencil(&stencil);
     graphics.draw_text("Bloop", 30, 20);
   }
 
   let mut copy = graphics.working_frame_bitmap();
 
-  for y in 20..30 {
-    for x in 10..20 {
-      copy.as_pixels_mut().set(x, y, PixelColor::BLACK);
+  for y in 0..240 {
+    for x in 0..400 {
+      let val = (x as f32 / 7.4).sin();
+      let val = val + 0.5 * (x as f32 / 3.4).sin();
+      let val = val + (y as f32 / 4.0).sin();
+      let val = val + 0.5 * (y as f32 / 2.2).sin();
+      copy.as_pixels_mut().set(
+        x,
+        y,
+        if val > 0f32 {
+          PixelColor::BLACK
+        } else {
+          PixelColor::WHITE
+        },
+      );
     }
   }
   graphics.draw_bitmap(&copy, 0, 30, BitmapFlip::kBitmapUnflipped);
 
-  let points = [
-    euclid::default::Point2D::new(10, 10),
-    euclid::default::Point2D::new(20, 20),
-    euclid::default::Point2D::new(10, 30),
-    euclid::default::Point2D::new(0, 20),
-  ];
-  graphics.fill_polygon(
-    &points,
-    Color::Solid(SolidColor::kColorBlack),
-    PolygonFillRule::kPolygonFillEvenOdd,
-  );
-
-  let c = graphics.bitmaps_collide(
-    BitmapCollider {
-      bitmap: &bmp,
-      flipped: BitmapFlip::kBitmapUnflipped,
-      x: 0,
-      y: 0,
-    },
-    BitmapCollider {
-      bitmap: &copy,
-      flipped: BitmapFlip::kBitmapUnflipped,
-      x: 0,
-      y: 0,
-    },
-    euclid::rect(0, 0, 100, 100),
-  );
-  log(format!("collision: {}", c));
-
   let id = graphics.push_context_bitmap(copy);
   graphics.pop_context();
-  let _copy = graphics.take_popped_context_bitmap(id).unwrap();
-
-  // working image
-  let yo_path = "images/yo";
-  let load = Bitmap::from_file(yo_path);
-  if let Ok(bitmap) = load {
-    graphics.draw_bitmap(&bitmap, 100, 80, BitmapFlip::kBitmapUnflipped);
-  }
-
-  // broken image
-  let broken_path = "images/wat";
-  let load = Bitmap::from_file(broken_path);
-  if let Err(error) = load {
-    log(error);
-  }
-
-  let display = &mut api.display;
-  display.set_inverted(true);
-  //display.set_flipped(true, false);
-  display.set_scale(2);
-
-  let list_files_in = |path: &str| match api.file.list_files(path) {
-    Ok(files) => {
-      log(format!("{}/ files:", path));
-      for fname in files {
-        log(format!("  {:?}", fname))
-      }
-    }
-    Err(e) => log(format!("ERROR: {}", e)),
-  };
-  let make_dir = |path: &str| match api.file.make_folder(path) {
-    Ok(()) => log(format!("mkdir {}", path)),
-    Err(e) => log(e),
-  };
-  let rename = |from: &str, to: &str| match api.file.rename(from, to) {
-    Ok(()) => {
-      log(format!("renamed {} to {}", from, to));
-      list_files_in("myfolder");
-    }
-    Err(e) => log(e),
-  };
-  let delete = |path: &str| match api.file.delete(path) {
-    Ok(()) => log(format!("deleted {} recursive", path)),
-    Err(e) => log(e),
-  };
-  let delete_recursive = |path: &str| match api.file.delete_recursive(path) {
-    Ok(()) => log(format!("deleted {} recursive", path)),
-    Err(e) => log(e),
-  };
-  let stat = |path: &str| match api.file.stat(path) {
-    Ok(stats) => log(format!("stat {}: {:?}", path, stats)),
-    Err(e) => log(e),
-  };
-  let write_file = |path: &str, stuff: &[u8]| match api.file.write_file(path, stuff) {
-    Ok(()) => log(format!("wrote {}", path)),
-    Err(e) => log(e),
-  };
-  let read_file = |path: &str| match api.file.read_file(path) {
-    Ok(content) => log(format!("read {}: {:?}", path, String::from_utf8(content))),
-    Err(e) => log(e),
-  };
-
-  list_files_in("images");
-
-  make_dir("myfolder");
-  make_dir("myfolder/two");
-  list_files_in("myfolder");
-  list_files_in("myfolder/two");
-
-  rename("myfolder/two", "myfolder/three");
-  stat("myfolder/three");
-
-  write_file("myfolder/three", b"bees\n");
-  write_file("myfolder/three/bears.txt", b"want honey\n");
-  read_file("myfolder/three/bears.txt");
-  read_file("myfolder/three/no_bears.txt");
-
-  delete("myfolder/three/no_bears.txt");
-  delete_recursive("myfolder");
-
-  let vol = api.sound.default_channel().volume();
-  log(format!("Default channel volume (in 0-1): {}", vol));
 
   let mut i32callbacks = Callbacks::<i32>::new();
-
-  let mut fileplayer = FilePlayer::from_file("mojojojo.pda").unwrap();
-  api.sound.default_channel_mut().add_source(&mut fileplayer).unwrap();
-  fileplayer.as_mut().set_completion_callback(
-    SoundCompletionCallback::with(&mut i32callbacks).call(|i| {
-      log(format!("finished playback of mojojojo {}", i));
-    }),
-  );
-  fileplayer.play(1).expect("fileplayer play failed?");
-  log(format!(
-    "Fileplayer length: {} seconds",
-    fileplayer.file_len().to_seconds(),
-  ));
-  fileplayer.fade_volume(
-    StereoVolume::zero(),
-    TimeDelta::from_seconds(1),
-    /*SoundCompletionCallback::with(&mut i32callbacks).call(|(_i, system)| {
-      log("fade done!");
-      log(">> getting time");
-      let t = system.current_time();
-      log("<< getting time");
-      log(format!("time {}", t));
-    }),*/
-    SoundCompletionCallback::none(),
-  );
-
-  let sample = AudioSample::with_bytes(100000);
-  let mut splayer = SamplePlayer::new(&sample);
-  splayer.play(1, 1.0);
-
-  struct GeneratorData {}
-  let data = GeneratorData {};
-  static VTABLE: SynthGeneratorVTable = SynthGeneratorVTable {
-    render_func: |_data, _r| false,
-    note_on_func: |_data, _note, _volume, _len| {},
-    release_func: |_data, _ended| {},
-    set_parameter_func: |_data, _parameter, _value| false,
-  };
-  let generator = SynthGenerator::new(data, &VTABLE);
-  let mut synth = Synth::new_with_generator(generator);
-  synth.play_frequency_note(0.0, 1.0.into(), None, None);
-  log(format!("synth playing: {}", synth.as_source().is_playing()));
-
-  let mut dline = DelayLine::new(TimeDelta::from_seconds(8), false);
-  let tap = dline.add_tap(TimeDelta::from_seconds(6)).unwrap();
-  api.sound.default_channel_mut().add_source(tap).unwrap();
-  dline.set_len(TimeDelta::from_seconds(3));
-  log(format!("dline length {}", dline.len()));
-
-  let _cb_source =
-    CallbackSource::new_mono_for_channel(api.sound.default_channel_mut(), |_buf: &mut [i16]| false);
-
-  let mut sequence = Sequence::from_midi_file("pirate.mid").unwrap();
-  for mut track in sequence.tracks_mut() {
-    let mut instrument = track.instrument_mut();
-    instrument.set_volume(StereoVolume::new(0.3, 0.3));
-    api.sound.default_channel_mut().add_source(&mut instrument).unwrap();
-
-    log(format!("polyphony: {}", track.polyphony()));
-    for _ in 0..track.polyphony() {
-      let mut synth = Synth::new_with_waveform(SoundWaveform::kWaveformSquare);
-      synth.set_attack_time(TimeDelta::from_milliseconds(0));
-      synth.set_decay_time(TimeDelta::from_milliseconds(200));
-      synth.set_sustain_level(0.3);
-      synth.set_release_time(TimeDelta::from_milliseconds(500));
-      instrument.add_voice(synth, MidiNoteRange::All, 0.0).unwrap();
-    }
-  }
-  sequence.play(SoundCompletionCallback::none());
-
-  let action_item = MenuItem::new_action(
-    "hello world",
-    MenuCallback::with(&mut i32callbacks).call(|i| {
-      log(format!("menu action {}", i));
-    }),
-  );
-  action_item.title();
-  let mut check_item = MenuItem::new_checkmark(
-    "dank",
-    false,
-    MenuCallback::with(&mut i32callbacks).call(|i| {
-      log(format!("dankness adjusted {}", i));
-    }),
-  );
-  check_item.set_checked(true);
-  let mut options_item = MenuItem::new_options(
-    "temp",
-    ["too hot", "too cold", "just right"],
-    MenuCallback::with(&mut i32callbacks).call(|i| {
-      log(format!("temperature adjusted {}", i));
-    }),
-  );
-  options_item.set_value(2);
-
-  api.sound.set_microphone_callback(
-    Some(|buf: &[i16]| {
-      assert!(false);
-      log(format!("mic data {:?}", buf));
-      MicrophoneCallbackOutput::ContinueRecording
-    }),
-    true,
-  );
-
-  match api.sound.headphone_state() {
-    HeadphoneState::HeadphoneNotConnected => log("headphone? no"),
-    HeadphoneState::HeadphoneConnected { has_microphone } => {
-      log(format!("headphone? yep! mic? {}", has_microphone))
-    }
-  };
 
   log(format!(
     "Entering main loop at time {}",
     api.system.current_time()
   ));
+
   let events = api.system.system_event_watcher();
+
+  // This is the main game
   loop {
     let (inputs, frame_number) = match events.next().await {
       SystemEvent::NextFrame {
@@ -327,6 +123,7 @@ async fn main(mut api: craydate::Api) -> ! {
       }
       _ => continue,
     };
+
     for (button, event) in inputs.buttons().all_events() {
       match event {
         craydate::ButtonEvent::Push => {
